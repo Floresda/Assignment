560198f9b7b203ec2706f70ab2acf1cce976d853
Floresda <97864447+Floresda@users.noreply.github.com>
2022-01-18 21:28:05 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+let prompter = prompt("What is your name?");
+let prompter1 = prompt("What is your favorite color?");
+let result = "My name is " + prompter + " and my favorite color is " + prompter1 + ".";
+document.body.innerHTML = result;
